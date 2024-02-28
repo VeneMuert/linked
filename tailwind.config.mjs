37a8a99d7411cb350b85e5536426cdcdd588100e
amitkaill <amitkaill1989@gmail.com>
2024-02-28 15:50:43 -0400

    colores

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
+	theme: {    
+	  colors:{
+			'Green': '#c5f82a',
+			'White': 'hsl(0, 0%, 100%)',
+			'Grey': 'hsl(0, 0%, 20%)',
+			'DarkGrey': 'hsl(0, 0%, 12%)',
+			'OffBlack': '#141414',
+	  },
+	  extend: {},
 	},
 	plugins: [],
-}
+  };
